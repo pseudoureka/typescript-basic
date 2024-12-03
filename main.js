2b951a6c97a4ea45f04cffe0c87ee72113f7e403
pseudoureka <messiah7179@gmail.com>
2024-12-03 21:31:53 +0900

    연습

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("안녕 타입스크립트");
+const current = [0, 0];
+const target = [4, 5];
+const dx = target[0] - current[0];
+const dy = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+const items = [];
+items.push("갑옷");
+items.push("빨간 물약");
+console.log(`${items.join(", ")}을/를 획득했다!`);
--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-console.log("안녕 타입스크립트");
+const current: [number, number] = [0, 0];
+const target: [number, number] = [4, 5];
+const dx = target[0] - current[0];
+const dy = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+
+const items: string[] = [];
+items.push("갑옷");
+items.push("빨간 물약");
+console.log(`${items.join(", ")}을/를 획득했다!`);
